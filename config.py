5948b225e432c36625342922518e22aee0b91318
M. Joranhezon <mjoranhezon@gmail.com>
2020-07-17 13:19:07 -0300

    Set up JWT token for user auth

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+from datetime import timedelta
 
 class BaseConfig:
     """Base configuration"""
@@ -8,6 +9,13 @@ class BaseConfig:
 class DevelopmentConfig(BaseConfig):
     """Development configuration"""
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(
+        minutes=int(os.getenv('JWT_ACCESS_TOKEN_EXPIRES'))
+    )
+    JWT_REFRESH_TOKEN_EXPIRES = timedelta(
+        minutes=int(os.getenv('JWT_REFRESH_TOKEN_EXPIRES'))
+    )
 
 class TestingConfig(BaseConfig):
     """Testing configuration"""
--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,8 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager
+from datetime import timedelta
 
 db = SQLAlchemy()
 
@@ -19,6 +21,16 @@ def create_app(script_info=None):
     # register blueprints
     from src.routes.auth import auth_blueprint
     app.register_blueprint(auth_blueprint)
+
+    # set up jwt token
+    jwt = JWTManager(app)
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(
+        minutes=int(os.getenv('JWT_ACCESS_TOKEN_EXPIRES'))
+    )
+    JWT_REFRESH_TOKEN_EXPIRES = timedelta(
+        minutes=int(os.getenv('JWT_REFRESH_TOKEN_EXPIRES'))
+    )
+    app.config['JWT_SECRET_KEY'] =  os.getenv('JWT_SECRET_KEY')
 
     # shell context for flask cli
     @app.shell_context_processor
