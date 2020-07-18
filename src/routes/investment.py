299aa8ab71e50919ecd2c0809b5fe5776f0d2b70
M. Joranhezon <mjoranhezon@gmail.com>
2020-07-18 13:57:48 -0300

    Create get investments route

--- src/routes/investment.py
+++ src/routes/investment.py
@@ -1,5 +1,6 @@
 import os
 import requests
+import json
 import sys
 from typing import List
 from flask import Blueprint
@@ -7,7 +8,50 @@ from flask import jsonify
 from flask import request
 from src import db
 from src.models import Investment
+from flask_jwt_extended import jwt_required
 investment_blueprint = Blueprint('investment_blueprint', __name__)
+
+"""
+Request Args: USD Value for how much user can spend on investment
+Response: List of investments
+Description: Returns a list of investments with value equal or below
+value sent by user
+"""
+
+
+@investment_blueprint.route('/get-investments', methods=['GET'])
+@jwt_required
+def get_investments():
+    usd_value = request.args.get('usd_value')
+
+    if usd_value is None:
+        return jsonify({
+            'message': 'Missing usd_value parameter'
+        }), 400
+
+    query = Investment.query.filter(Investment.usd_value <= usd_value).all()
+
+    if len(query) < 1:
+        return jsonify({
+            'Message': 'There is no stock that fits your value range'
+        }), 400
+
+    count = 0
+    stock_list = []
+    for stock in query:
+        temp_stock = {
+            'name': stock.stock_name,
+            'usd_value': stock.usd_value,
+            'bitcoin_value': stock.bitcoin_value,
+            'is_bitcoin': stock.is_bitcoin
+        }
+
+        stock_list.append(temp_stock)
+
+    return jsonify({
+        'stocks': stock_list,
+    }), 200
+
 
 """
 Request Args: None
@@ -97,3 +141,33 @@ def post_bitcoin():
     return jsonify({
         'Message': 'Stored bitcoin sucessfully'
     }), 200
+
+
+"""
+Request Body: None
+Response: Status code determining if content of investments table was deleted
+Description: Used to delete all entries in investments table
+"""
+
+
+@investment_blueprint.route('/delete-investments', methods=['DELETE'])
+def delete():
+    query = Investment.query.order_by(Investment.stock_name).all()
+
+    if len(query) >= 1:
+        try:
+            for stock in query:
+                db.session.delete(stock)
+            db.session.commit()
+        except Exception as e:
+            return jsonify({
+                'message': 'Error in database, try again!'
+            }), 400
+
+        return jsonify({
+            'message': 'Records sucessfully deleted'
+        }), 200
+    else:
+        return jsonify({
+            'message': 'No records to delete'
+        }), 400
--- test/test_investment.py
+++ test/test_investment.py
@@ -2,7 +2,8 @@ import os
 import unittest
 import requests
 import json
- 
+import sys
+from flask import jsonify
 from src import create_app, db
 import config
 
@@ -20,8 +21,10 @@ class InvestmentTests(unittest.TestCase):
         self.app = app.test_client()
         self.app.testing = True
 
+    # Delete users created in tests
     def tearDown(self):
-        pass
+        self.app.delete('/delete')
+        self.app.delete('/delete-investments')
 
     """
     Commented because using it repeatedly could exaust api's free trial
@@ -33,6 +36,69 @@ class InvestmentTests(unittest.TestCase):
     def test_should_populate_database_with_bitcoin(self):
         response = self.app.post('/populate-bitcoin')
         self.assertEqual(response.status_code, 200)
+
+    def test_should_get_stocks_below_usd_value(self):
+        # Get JWT Token and populate investments table
+        JWT_TOKEN = signup_and_login(self.app)
+        self.app.post('/populate-bitcoin')
+
+        headers = {
+            'Authorization': 'Bearer ' + JWT_TOKEN
+        }
+
+        params = {
+            'usd_value': 500
+        }
+
+        response = self.app.get('/get-investments', headers=headers, query_string=params)
+        self.assertEqual(response.status_code, 200)
+
+    def test_should_not_get_stocks_if_there_are_none(self):
+        # Get JWT Token
+        JWT_TOKEN = signup_and_login(self.app)
+
+        headers = {
+            'Authorization': 'Bearer ' + JWT_TOKEN
+        }
+
+        params = {
+            'usd_value': 500
+        }
+
+        response = self.app.get('/get-investments', headers=headers, query_string=params)
+        self.assertEqual(response.status_code, 400)
+
+    def test_should_not_get_stocks_without_jwt_token(self):
+        params = {
+            'usd_value': 500
+        }
+
+        response = self.app.get('/get-investments', query_string=params)
+        self.assertEqual(response.status_code, 401)
+
+    def test_should_not_get_stocks_without_usd_value(self):
+        JWT_TOKEN = signup_and_login(self.app)
+
+        headers = {
+            'Authorization': 'Bearer ' + JWT_TOKEN
+        }
+
+        response = self.app.get('/get-investments', headers=headers)
+        self.assertEqual(response.status_code, 400)
+
+# Sign up and log in to get JWT Token   
+def signup_and_login(client):
+    payload = {
+        'username': 'marina',
+        'email': 'marina@gmail.com',
+        'password': 'senha123'
+    }
+    headers = {'content-type': 'application/json'}
+
+    response = client.post('/sign-up', json=payload)
+    response = client.post('/login', json=payload)
+
+    return response.json['JWT_Token']
 
 if __name__ == "__main__":
     unittest.main()
